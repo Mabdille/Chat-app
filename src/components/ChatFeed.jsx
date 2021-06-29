9f0e9ccdb4f31bc4afb765c042ebfc6a81ce52cf
Mabdille <mabdille97@gmail.com>
2021-06-29 20:14:13 +0100

    no message

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,6 +1,36 @@
+import MessageForm from "./MessageForm"
+import MyMessage from "./MyMessage"
+import TheirMessage from "./TheirMessage"
+
 const ChatFeed = (props) => {
-    console.log(props)
+ const {chats, activeChats, userName, messages } = props
     
+    const chat = chats && chats[activeChat]
+
+    console.log(chat, userName, messages)
+    const renderMessages = () => {
+        const keys = Object.keys(messages)
+        return keys.map((key, index => {
+            const message = message[key]
+            const lastMessageKey = index === 0 ? null : keys[index - 1]
+            const isMyMessage = userName === message.sender.username
+
+            return (
+                <div key= {`msg_${index}`} style={{width: '100%' }}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                                ? <MyMessage />
+                                : <TheirMessage />
+                }
+                    </div>
+                    <div className="read-receipts" style={{ marginRight: isMyMessage ? }}>
+                    </div>
+                </div>
+            )
+            
+        })
+    }
     return (
         <div>
             ChatFeed
