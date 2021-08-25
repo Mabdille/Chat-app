2f935d653041cc62b6d0f916f15a314ba9dd7e10
Mabdille <mabdille97@gmail.com>
2021-08-25 19:31:47 +0100

    no message

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const authObject = { 'Project-ID': "3726960b-2239-4651-9d35-5b2ac1b74f0c"}
+
+        // usernam / password => chatengine -> give messages
+        // works out -> logged in 
+        // error -> try with new username
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+                    <input  type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required/>
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+                        </button>
+
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    )
+}
